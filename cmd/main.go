a78e117e8db97a28bb82aa371f6b7e1270e96ba4
lva100 <lva100@yandex.ru>
2025-04-08 10:24:50 +0300

    Decomposition routing and add config service

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,21 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"log"
+	"lva100/go-fiber/config"
+	"lva100/go-fiber/internal/home"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
+	config.Init()
 	app := fiber.New()
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, world!!!")
-	})
+	dbConf := config.NewDatabaseConfig()
+	log.Println(dbConf)
+	// app.Get("/", func(c *fiber.Ctx) error {
+	// 	return c.SendString("Hello, world!!!")
+	// })
+	home.NewHandler(app)
 	app.Listen(":3000")
 }
