8abc522fc0ea2727390d177f220f0ad08dd3561e
lva100 <lva100@yandex.ru>
2025-04-10 14:50:46 +0300

    Local vars and functions in templates

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"lva100/go-fiber/config"
 	"lva100/go-fiber/internal/home"
 	"lva100/go-fiber/pkg/logger"
+	"strings"
 
 	"github.com/gofiber/contrib/fiberzerolog"
 	"github.com/gofiber/fiber/v2"
@@ -17,6 +18,11 @@ func main() {
 	logConfig := config.NewLogConfig()
 	customLogger := logger.NewLogger(logConfig)
 	engine := html.New("./html", ".html")
+	engine.AddFuncMap(map[string]interface{}{
+		"ToUpper": func(c string) string {
+			return strings.ToUpper(c)
+		},
+	})
 	app := fiber.New(fiber.Config{
 		Views: engine,
 	})
